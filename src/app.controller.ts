8f2c163c71625712f14a1afaa6678dacc056add9
Lisa Han <ellesis@gmail.com>
2022-11-14 22:23:55 -0800

    add endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // @Get('/:id')
+  // getOne(@Param("id") movieId: string) { // @Param() 여기서 받아온 값이 movieId라는 변수에 들어가게 된다.
+  //   return `This will return one movie with the id:${movieId}`;
+  // }
+
+  // @Post()
+  // @Delete('/:id')
+  // @Put()
+  // @Patch('/:id')
 }
